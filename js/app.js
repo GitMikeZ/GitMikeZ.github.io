64b975c04f1d41253002667ef96b01a2c22d2683
Mike Z <minqi03@gmail.com>
2017-06-17 20:10:08 -0700

    app

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+
+window._wapiq = window._wapiq || [];
+
+function videoObject () {
+		_wapiq.push(function(W) {
+			window.wistiaUploaderButtonOnly = new W.Uploader({
+				accessToken: "7dc7c6050850b2a4fa386519473f06574e540ae1bbf587172a812f1cff7b7c1d",
+				button: "wistia_upload_button_button_only",
+				projectId: "bdse3s7d07"
+			});
+		});
+}
